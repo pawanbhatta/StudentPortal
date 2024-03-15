44c2d5e1956fb92ba4a0717036745ba00c040f1c
Pawan Bhatta <pawanbhatta00@gmail.com>
2024-03-15 09:57:42 +0545

    create student operation done

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.Data;
+using StudentPortal.Models;
+using StudentPortal.Models.Entities;
+
+namespace StudentPortal.Controllers
+{
+	public class StudentsController : Controller
+	{
+		private readonly ApplicationDbContext dbContext;
+
+		public StudentsController(ApplicationDbContext dbContext)
+        {
+			this.dbContext = dbContext;
+		}
+
+        [HttpGet]
+		public IActionResult Add()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+		{
+			var student = new Student
+			{
+				Name = viewModel.Name,
+				Email = viewModel.Email,
+				Phone = viewModel.Phone,
+				Subscribed = viewModel.Subscribed
+			};
+			await dbContext.Students.AddAsync(student);
+			await dbContext.SaveChangesAsync();
+
+			return View();
+		}
+	}
+}
